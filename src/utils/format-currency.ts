8596dfd5f9ea0b2d8114265cb1c3d05a4d8525c1
João Oliveira <joaoliveira.batista1@gmail.com>
2026-07-19 21:39:41 -0300

    feat: add utility function to format currency

---
+++ src/utils/format-currency.ts
@@ -0,0 +1,8 @@
+export function formatCurrency(value: string | number): string {
+  return value.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  })
+}
