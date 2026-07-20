24659d680d45e32a612bf1aee9a60583b4b6873e
João Oliveira <joaoliveira.batista1@gmail.com>
2026-07-20 08:27:05 -0300

    feat: add context to cart

---
+++ src/contexts/cart-context.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { createContext, type ReactNode, useContext, useState } from 'react'
+
+interface CartItem {
+  productId: string
+  quantity: number
+}
+
+interface ICartContext {
+  items: CartItem[]
+  addToCart: (productId: string) => void
+}
+
+const CartContext = createContext({} as ICartContext)
+
+export function CartProvider({ children }: Readonly<{ children: ReactNode }>) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([])
+
+  function addToCart(productId: string) {
+    setCartItems(prev => {
+      const productInCart = prev.some(item => item.productId === productId)
+
+      if (productInCart) {
+        return prev.map(item => {
+          if (item.productId === productId) {
+            return { ...item, quantity: item.quantity + 1 }
+          }
+
+          return item
+        })
+      }
+
+      return [...prev, { productId, quantity: 1 }]
+    })
+  }
+
+  return (
+    <CartContext.Provider value={{ items: cartItems, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+export function useCartContext() {
+  const context = useContext(CartContext)
+
+  if (!context) {
+    throw new Error('useCartContext must be used within a CartProvider')
+  }
+
+  return context
+}
