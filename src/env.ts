a0b04e23bb51164b4e0c5729533a3454685177a9
João Oliveira <joaoliveira.batista1@gmail.com>
2026-07-19 21:35:53 -0300

    feat: add validation for environment variables

---
+++ src/env.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NEXT_PUBLIC_API_BASE_URL: z.url({
+    error: 'NEXT_PUBLIC_API_BASE_URL must be a valid url',
+  }),
+})
+
+export const env = envSchema.parse(process.env)
