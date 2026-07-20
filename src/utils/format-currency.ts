5cca6e05fb60caf2eb90bd2b89f02aa47f4e84c8
João Oliveira <joaoliveira.batista1@gmail.com>
2026-07-19 23:18:06 -0300

    feat: enhance formatCurrency function to accept options for customization

--- src/utils/format-currency.ts
+++ src/utils/format-currency.ts
@@ -1,8 +1,12 @@
-export function formatCurrency(value: string | number): string {
+export function formatCurrency(
+  value: string | number,
+  options?: Intl.NumberFormatOptions
+): string {
   return value.toLocaleString('pt-BR', {
     style: 'currency',
     currency: 'BRL',
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
+    ...options,
   })
 }
