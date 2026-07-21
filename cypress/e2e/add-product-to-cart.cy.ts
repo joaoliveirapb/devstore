3cd66214f75937ba6e025d750b94fe1f1525dce3
João Oliveira <joaoliveira.batista1@gmail.com>
2026-07-20 23:20:05 -0300

    test: add e2e tests for adding products to cart

---
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -0,0 +1,38 @@
+describe('Add product to cart', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('should be able to navigate to the product page and add it to the cart', () => {
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.url().should('include', '/product')
+
+    cy.contains('Adicionar ao carrinho').click()
+
+    cy.contains('Carrinho (1)').should('exist')
+  })
+
+  it('should not count duplicated products on cart', () => {
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.url().should('include', '/product')
+
+    cy.contains('Adicionar ao carrinho').click()
+    cy.contains('Adicionar ao carrinho').click()
+
+    cy.contains('Carrinho (1)').should('exist')
+  })
+
+  it('should be able to search for a product and add it to the cart', () => {
+    cy.searchByQuery('camiseta')
+
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.url().should('include', '/product')
+
+    cy.contains('Adicionar ao carrinho').click()
+
+    cy.contains('Carrinho (1)').should('exist')
+  })
+})
